deb645de56cc238cc76601513186887c124aece1
Hayk Avagyan <hayk.avagyan.a@gmail.com>
2021-01-12 14:09:21 +0400

    Better fit for text and responsive images(looks good on mobile now).

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 	var width = $(window).width();
-	width /= 12;
+	width /= 13;
 	
 	$(".content").css("padding-right", width.toString() + "px");
 
 	$(window).resize(function () {
 		var width = $(window).width();
-		width /= 12;
+		width /= 13;
 		$(".content").css("padding-right", width.toString() + "px");
 	});
 });
