efb0d85121be686e537b1201f7b1a04cc244fc8c
Hayk Avagyan <hayk.avagyan.a@gmail.com>
2021-01-12 14:53:16 +0400

    go back

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 	var width = $(window).width();
-	var height = $(window).height();
 	width /= 13;
 	
 	$(".content").css("padding-right", width.toString() + "px");
@@ -9,7 +8,5 @@ $(document).ready(function () {
 		var width = $(window).width();
 		width /= 13;
 		$(".content").css("padding-right", width.toString() + "px");
-		
-		$("body").css("height", height.toString() + "px");
 	});
 });
