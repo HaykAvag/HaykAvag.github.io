448609fa8fcbb43216704219b573822e21bdd9a9
Hayk Avagyan <hayk.avagyan.a@gmail.com>
2021-01-12 14:31:28 +0400

    responsive height fix

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,7 @@ $(document).ready(function () {
 		var width = $(window).width();
 		width /= 13;
 		$(".content").css("padding-right", width.toString() + "px");
+		
+		$("body").css("height", "100vh");
 	});
 });
